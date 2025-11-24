b425a77b45181ff99a0441db1e3b774967054c51
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2025-11-24 14:45:35 -0300

    criação arquivos de tema e structs

---
+++ src/structs/structs.h
@@ -0,0 +1,58 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+
+// Permissão 0 => Usuário Comum
+// Permissão 1 => Usuário Administrador
+
+// Estrutura Usuário
+struct Usuario {
+    int id;
+    char nome[32];
+    char email[32];
+    int permissao;
+};
+
+// Estrutura Livro
+struct Livro {
+    int id;
+    char nome[32];
+    char categoria[16];
+    char autor[32];
+};
+
+// Estrutura Calculadora
+struct Calculadora {
+    int id;
+    char modelo[8];
+    char marca[8];
+};
+
+// Estrutura Tablet
+struct Tablet {
+    int id;
+    char modelo[8];
+    char marca[8];
+};
+
+// Estrutura Fone de Ouvido
+struct Fone_Ouvido {
+    int id;
+    char modelo[8];
+    char marca[8];
+};
+
+// Estrutura de Empréstimo
+struct Emprestimo {
+    int id;
+    int id_recurso;
+    char nome_recurso[32];
+
+    int id_usuario;
+    char nome_usuario[32];
+
+    char data_devolucao[8];
+    float valor_multa;
+};
+
+#endif
---
+++ src/themes/theme.h
@@ -0,0 +1,21 @@
+#ifndef THEME_H
+#define THEME_H
+
+#define SUCCESS "\x1b[1;32m"
+
+// Vermelho Negrito (para Erros)
+#define ERROR  "\x1b[1;91m"
+
+// Amarelo Negrito (para Avisos, Listas)
+#define HELP   "\x1b[1;93m"
+
+// Azul Negrito (para Dados, Infos)
+#define OUTPUT "\x1b[1;94m"
+
+// Cinza Negrito (para Prompts, Comandos)
+#define INPUT  "\x1b[1;90m"
+
+// Código para resetar a cor de volta ao padrão do terminal
+#define RESET  "\x1b[0m"
+
+#endif
