8ea402f82932a8f1617a91df366a2c773ef743b3
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2025-11-25 21:30:54 -0300

    removendo importações desnecessárias

--- main.c
+++ main.c
@@ -2,10 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "src/structs/structs.h"
-#include "src/themes/theme.h"
 #include "src/interfaces/interfaces.h"
-#include "src/manipulacao/arquivo.h"
 
 int main(){ 
 
